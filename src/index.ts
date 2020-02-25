b8a07981478676958c9f16b3a1528de1a748612e
Alexander Schelchere <ascellie@gmail.com>
2020-02-25 09:11:57 -0500

    mongoose connection secured

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express, { Request, Response } from 'express';
+import db from './database/db';
 
 const server = express();
+
+// middleware
 server.use(cors());
+
+// database connection
+db.on('error', (err) => console.error(err));
+db.once('open', () => console.log('connected to MongoDB Database'));
 
 const port = process.env.PORT || 3001;
 
