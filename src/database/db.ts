78942399a100d7b73286c2f0f26762f80481d714
Alexander Schelchere <ascellie@gmail.com>
2020-02-25 13:29:41 -0500

    cleanup mongodb connection file

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,7 +1,10 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
-mongoose.connect(`${process.env.MONGODB_CONNECTION_URL}`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`${process.env.MONGODB_CONNECTION_URL}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 
 export default db;
