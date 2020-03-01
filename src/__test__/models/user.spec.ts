2c946765738a6b9d7928e2feb284b941b7540f7d
Alexander Schelchere <ascellie@gmail.com>
2020-03-01 11:36:09 -0500

    test user

--- src/__test__/models/user.spec.ts
+++ src/__test__/models/user.spec.ts
@@ -1,38 +1,29 @@
+// import the user models
+import User, { IUserDoc } from '../../models/user';
 // import deps
 import { expect } from 'chai';
-// import the user models
-import User from '../../models/user';
 
-let testUser1;
-let testUser2;
+const [existingUserDetails, newUserDetails] = [
+  {
+    username: 'jim',
+    firstName: 'jimmy',
+    lastName: 'neutron',
+    email: 'jimmy@gotta.blast',
+  },
+  {
+    username: 'tim',
+    firstName: 'timmy',
+    lastName: 'turner',
+    email: 'timmy@i.wish',
+  },
+];
 
-beforeEach(done => {
-  const [userDetails1, userDetails2] = [
-    {
-      username: 'jim',
-      firstName: 'jimmy',
-      lastName: 'neutron',
-      email: 'jimmy@gotta.blast',
-    },
-    {
-      username: 'tim',
-      firstName: 'timmy',
-      lastName: 'turner',
-      email: 'timmy@i.wish',
-    },
-  ];
-  testUser1 = new User(userDetails1);
-  testUser2 = new User(userDetails2);
-  testUser1.save();
-  testUser2.save();
-  Promise.all([testUser1, testUser2]).then(() => done());
-});
+// run this code after we start any of our tests
 
-describe('USER model', () => {
-  it('should be able to read name', done => {
-    User.findOne({ username: 'jim' }).then(user => {
-      if (user) expect(user.firstName).to.eql('jimmy');
-    });
-    done();
+describe('User model', () => {
+  it('should be able to create a new user', () => {
+    let newUser = new User(newUserDetails);
+    newUser.save();
+    expect(newUser.username).to.eql('tim');
   });
 });
